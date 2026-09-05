26451e930ec51e8a087a5adef0587a793e000272
x0c <34805149+x0c@users.noreply.github.com>
2026-09-05 16:31:58 +0800

    cpu-killer 1.0.11

--- Casks/cpu-killer.rb
+++ Casks/cpu-killer.rb
@@ -1,6 +1,6 @@
 cask "cpu-killer" do
-  version "1.0.9"
-  sha256 "702fb4b4994f9732c211fdaa65cc2b5910556cc864cad809335907723011c61a"
+  version "1.0.11"
+  sha256 "1d66e190d85b0ddf302efdbccf3e81b7000d4f2a98893b0671da7773d11f919d"
 
   url "https://github.com/x0c/CPUKiller/releases/download/v#{version}/CPU-Killer-#{version}.dmg"
   name "CPU Killer"

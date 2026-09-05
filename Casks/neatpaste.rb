7f74a15cdb0fe88e0281383a1750dc228c3e0563
x0c <34805149+x0c@users.noreply.github.com>
2026-09-05 18:17:49 +0800

    neatpaste 1.0.5

--- Casks/neatpaste.rb
+++ Casks/neatpaste.rb
@@ -1,6 +1,6 @@
 cask "neatpaste" do
-  version "1.0.4"
-  sha256 "f9a63dd83b02b9f13b2826914c5cd20ab03466ee60f435de27b96d5223822b7d"
+  version "1.0.5"
+  sha256 "14d0122b733427161fb87b4d1f593ee6c867c2d8cc9a09c577275fd31f3ac6d4"
 
   url "https://github.com/NeatMacApps/NeatPaste/releases/download/v#{version}/NeatPaste-#{version}.dmg"
   name "NeatPaste"

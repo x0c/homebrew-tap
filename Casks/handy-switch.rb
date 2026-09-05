c3e0c8812b1494ff757b69bbb4aeb13a3201e691
x0c <34805149+x0c@users.noreply.github.com>
2026-09-05 23:25:25 +0800

    handy-switch 1.0.11

--- Casks/handy-switch.rb
+++ Casks/handy-switch.rb
@@ -1,6 +1,6 @@
 cask "handy-switch" do
-  version "1.0.10"
-  sha256 "42c72387ce5280e6a8873ba4428216e4a07653c8a26b46687acad982bde04504"
+  version "1.0.11"
+  sha256 "ced1c2f4db413acbb7c7ceaf24a436bdb1118935638b680d93cec5f370a9f9a0"
 
   url "https://github.com/x0c/HandySwitch/releases/download/v#{version}/HandySwitch-#{version}.dmg"
   name "HandySwitch"
